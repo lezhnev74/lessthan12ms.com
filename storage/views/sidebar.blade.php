d16b03497ad25948bb75c2354e083ed56d7da993
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2019-10-21 12:53:03 +0500

    add link to the git repo

--- storage/views/sidebar.blade.php
+++ storage/views/sidebar.blade.php
@@ -9,7 +9,12 @@
                 @yield('post')
                 <br>
                 <br>
-                <a href="/atom.xml">ATOM feed</a>
+                <br>
+                <br>
+                <a href="/atom.xml">ATOM feed</a> |
+                <a href="https://github.com/lezhnev74/lessthan12ms.com">Website source code</a>
+                <br>
+                <br>
             </div>
         </div>
     </div>
