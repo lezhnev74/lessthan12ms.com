01db5d9899809a329ed4c996c19a60ad280a0f82
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2020-03-03 17:49:33 +0500

    Remove contracting label as it is not relevant atm

--- storage/views/header.blade.php
+++ storage/views/header.blade.php
@@ -15,7 +15,7 @@
             <a href="https://8thlight.com/blog/uncle-bob/2012/08/13/the-clean-architecture.html">Clean Architecture</a>
             advocate<br>
         </div>
-        <img src="/img/badge.svg" title="available for contracting">
+{{--        <img src="/img/badge.svg" title="available for contracting">--}}
 
         <br><br>
 
